18b177400b201f4f8bef67a3c6a134f3ef4d4210
Hayden Roney <roneyhayden@gmail.com>
2024-01-14 01:39:04 -0600

    project start createreactapp and client routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import { Outlet, Navigate, useNavigate } from "react-router-dom"
+import NavBar from "./components/NavBar"
 
 function App() {
+  const navigate = useNavigate();
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <NavBar />
+      {/* <header>
+        This is the header
       </header>
+      <main>
+        This is the Main
+      </main>
+      <footer>
+        This is the Footer
+      </footer> */}
     </div>
   );
 }
